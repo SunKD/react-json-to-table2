bd60c4d1f14e9ed86736bbb3be8221a60086604a
Sun Davey <v-sundav@microsoft.com>
2020-07-15 21:20:08 -0700

    excluding css

--- build/lib/components/index.js
+++ build/lib/components/index.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 // import the build index.css
-require("./index.css");
 // export our default example component
 var JsonToTable_1 = require("./JsonToTable");
 exports.JsonToTable = JsonToTable_1.default;
